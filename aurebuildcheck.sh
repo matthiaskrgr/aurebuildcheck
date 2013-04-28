97d9e128a7a8b7543c76b9fb414f707a03249847
Matthias Krüger <matthias.krueger@famsik.de>
2013-04-28 23:30:34 +0200

    Add working version of the script.

--- aurebuildcheck.sh
+++ aurebuildcheck.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+echo "Running 'findbrokenpkgs'."
+echo "This may take some time..."
+
+LOCALPKGS=`pacman -Qqm`
+BROKENPKGS=`findbrokenpkgs -q -nc | sed '3q;d'`
+NEEDSREBUILD=""
+
+for BROKENPKG in ${BROKENPKGS}; do
+	for LOCALPKG in ${LOCALPKGS} ; do
+		if [[ "${BROKENPKG}" == "${LOCALPKG}" ]] ; then
+			NEEDSREBUILD="${NEEDSREBUILD} ${LOCALPKG}"
+		fi
+	done
+done
+
+if [ -z ${NEEDSREBUILD} ] ; then
+	echo "All local packages seem ok."
+else
+	echo "Local packages that may need rebuild:"
+	echo "${NEEDSREBUILD}"
+fi
