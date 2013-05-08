9e4445875eb215a7429ecdd2823812467cb6553b
Matthias Krüger <matthias.krueger@famsik.de>
2013-05-08 14:55:21 +0200

    Major rewrite. Instead of relying on lddd, the script itself will now only check the files of local packages instead of all packages and sorting non-local ones out afterwards. This is much faster and more reliable than lddd.

--- aurebuildcheck.sh
+++ aurebuildcheck.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+export LANG=C
+
+RED='\e[1;31m'
+GREEN='\e[3;32m'
+REDUL='\e[4;31m'
+WHITEUL='\e[4;02m'
+NC='\e[0m'
+brokenpkgs=""
+
 timestart() {
 	TS=`date +%s.%N`
 }
@@ -9,40 +18,39 @@ timeend() {
 	TD=`calc -p $TE - $TS`
 }
 
-echo "Running 'lddd'."
-#echo "This may take some time..."
+
+
+echo "Checking local packages..."
 
 timestart
+localpackages=`pacman -Qqm`
+localpackagesamount=`echo $localpackages | wc -w`
+echo -e "${localpackagesamount} packages will be checked...\n"
 
-lddd
-
-echo "Reading files and stuff..."
-echo ""
-LOCALPKGS=`pacman -Qqm`
-DIR=`ls -tl /tmp/ | grep "ldd"  | head -n1 | awk '{print "/tmp/"$9}'`
-BROKENPKGS=`cat "${DIR}"/possible-rebuilds.txt | awk '{print $2}'`
-NEEDSREBUILD=""
-
-for BROKENPKG in ${BROKENPKGS}; do
-	for LOCALPKG in ${LOCALPKGS} ; do
-		if [[ "${BROKENPKG}" == "${LOCALPKG}" ]] ; then
-			NEEDSREBUILD="${NEEDSREBUILD} ${LOCALPKG}"
+brokenpkgs=""
+localpackages=`pacman -Qqm`
+for package in $localpackages ; do
+	echo "checking ${package}..."
+	packagefiles=`pacman -Qql $package | grep -v "\.a\|\.png\|\.la\|\.ttf\|\.gz\|\.html\|\.css\|\.h\|\.xml\|\.rgb\|\.gif\|\.wav\|\.ogg\|\.mp3\|\.po\|\.txt\|\.jpg\|\.jpeg"`
+	IFS=$'\n'
+	for file in $packagefiles; do
+		if (( $(file $file | grep -c 'ELF') != 0 )); then
+			#  Is an ELF binary.
+			if (( $(ldd $file 2>/dev/null | grep -c 'not found') != 0 )); then
+				#  Missing lib.
+				echo -e "\t ${RED}${file}${NC} ${REDUL}`ldd $file 2>/dev/null | grep 'not found'`${NC}" # >> $TEMPDIR/raw.txt
+				brokenpkgs="${brokenpkgs} ${package}"
+			fi
 		fi
 	done
 done
+echo "everything done."
 
-if [ -z ${NEEDSREBUILD} ] ; then
-	echo "All local packages seem ok."
-else
-	echo "Local packages that may need rebuild:"
-	echo "${NEEDSREBUILD}"
-fi
+echo -e "\n\nPackages which may need rebuild: \n ${RED}${brokenpkgs}${NC}\n"
 
 timeend
 TIME=`awk 'match($0,/[0-9]*.[0-9]{5}/) {print substr($0,RSTART,RLENGTH)}' <( echo "${TD}" )`
 
 echo "Done after ${TIME} seconds."
 
-echo "lddd may not be able to distinguish between required and optional dependencies!"
-echo "Removing files... ( ${DIR} )"
-rm -r ${DIR}
+echo "This script may not be able to distinguish between required and optional dependencies!"
