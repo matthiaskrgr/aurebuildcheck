28512522db6e949fae02d2bdb0b1ab4518e4b7a1
Matthias Krüger <matthias.krueger@famsik.de>
2013-05-15 20:15:43 +0200

    check if a package exists before trying to find out if deps are broken.

--- aurebuildcheck.sh
+++ aurebuildcheck.sh
@@ -41,31 +41,35 @@ fi
 
 
 for package in $localpackages ; do
-	BROKEN="false"
-	printf "checking ${package}..."
-	# sort out some files which are not supposed to be ELF files anyway.
-	packagefiles=`pacman -Qql $package | grep -v "\/$\|\.a\|\.png\|\.la\|\.ttf\|\.gz\|\.html\|\.css\|\.h\|\.xml\|\.rgb\|\.gif\|\.wav\|\.ogg\|\.mp3\|\.po\|\.txt\|\.jpg\|\.jpeg\|\.bmp\|\.xcf\|\.mo\|\.rb\|\.py\|\.lua\|\.config\|\.svg\|\.desktop\|\.conf\|\.pdf"`
-	IFS=$'\n'
-	for file in $packagefiles; do # check the files
-		if (( $(file $file | grep -c 'ELF') != 0 )); then
-			#  Is an ELF binary.
-			libs=`readelf -d "${file}" | awk '/NEEDED.*\[.*\]/''{print $5}' | awk  '{ gsub(/\[|\]/, "") ; print  }'`
-			for lib in ${libs} ; do
-			# needed libs
-				if [ -z `whereis ${lib} | awk '{print $2}'` ] ; then
-					#  Missing lib.
-					printf "\n\t ${RED}${file}${NC} needs ${REDUL}${lib}${NC}"
-					BROKEN="true" # to avoid packages being listed in the brokenpkg array several times
-				fi
-			done
-		fi
-	done
+	if [ `pacman -Qq $package 2>1` ] ; then
+		BROKEN="false"
+		printf "checking ${package}..."
+		# sort out some files which are not supposed to be ELF files anyway.
+		packagefiles=`pacman -Qql $package | grep -v "\/$\|\.a\|\.png\|\.la\|\.ttf\|\.gz\|\.html\|\.css\|\.h\|\.xml\|\.rgb\|\.gif\|\.wav\|\.ogg\|\.mp3\|\.po\|\.txt\|\.jpg\|\.jpeg\|\.bmp\|\.xcf\|\.mo\|\.rb\|\.py\|\.lua\|\.config\|\.svg\|\.desktop\|\.conf\|\.pdf"`
+		IFS=$'\n'
+		for file in $packagefiles; do # check the files
+			if (( $(file $file | grep -c 'ELF') != 0 )); then
+				#  Is an ELF binary.
+				libs=`readelf -d "${file}" | awk '/NEEDED.*\[.*\]/''{print $5}' | awk  '{ gsub(/\[|\]/, "") ; print  }'`
+				for lib in ${libs} ; do
+				# needed libs
+					if [ -z `whereis ${lib} | awk '{print $2}'` ] ; then
+						#  Missing lib.
+						printf "\n\t ${RED}${file}${NC} needs ${REDUL}${lib}${NC}"
+						BROKEN="true" # to avoid packages being listed in the brokenpkg array several times
+					fi
+				done
+			fi
+		done
 
-	if [[ ${BROKEN} == "true" ]] ; then
-		printf "\n"
-		brokenpkgs="${brokenpkgs} ${package}"
-	elif [[ ${BROKEN} == "false" ]] ; then
-		printf " ${GREEN}ok${NC}\n"
+		if [[ ${BROKEN} == "true" ]] ; then
+			printf "\n"
+			brokenpkgs="${brokenpkgs} ${package}"
+		elif [[ ${BROKEN} == "false" ]] ; then
+			printf " ${GREEN}ok${NC}\n"
+		fi
+	else
+		echo -e "${RED}Warning: '${package}' not found!${NC}"
 	fi
 done
 echo "everything done."
