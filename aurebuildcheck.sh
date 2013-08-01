5d0402da5987a63193446c8d46174ddb15386bd0
Matthias Krüger <matthias.krueger@famsik.de>
2013-08-01 15:17:03 +0200

    revert back to using LDD. Seems to get rid of some code, is faster and not broken which the whereis check was.

--- aurebuildcheck.sh
+++ aurebuildcheck.sh
@@ -49,22 +49,13 @@ for package in $localpackages ; do
 		IFS=$'\n'
 		for file in $packagefiles; do # check the files
 			if (( $(file $file | grep -c 'ELF') != 0 )); then
-				#  Is an ELF binary.
-				rpath=`readelf -d "${file}" | awk '/RPATH.*\[.*\]/''{print $5}' | awk  '{ gsub(/\[|\]/, "") ; print  }'`
-				libs=`readelf -d "${file}" | awk '/NEEDED.*\[.*\]/''{print $5}' | awk  '{ gsub(/\[|\]/, "") ; print  }'`
-				for lib in ${libs} ; do
-				# needed libs
-					if [ -z `whereis ${lib} | awk '{print $2}'` ] ; then
-						if [ -n `readelf -d ${file} | grep -o "RPATH"` ] ; then # check if there is a rpath, if yes, check if it leads to a lib
-							rpath=`readelf -d "${file}" | awk '/RPATH.*\[.*\]/''{print $5}' | awk  '{ gsub(/\[|\]/, "") ; print  }'`
-							if [ ! -e "${rpath}/${lib}" ] ; then # check if rpath files does not exists, if true, pkg broken
-								#  Missing lib.
-								printf "\n\t ${RED}${file}${NC} needs ${REDITALIC}${lib}${NC}"
-								BROKEN="true" # to avoid packages being listed in the brokenpkg array several times
-							fi
-						fi
-					fi
-				done
+			# is an ELF binary
+				lib=`ldd $file 2>1 | grep "\ =>\ not\ found" | awk '{print $1}'`
+				if [ ! -z ${lib} ] ; then
+					printf "\n\t ${RED}${file}${NC} needs ${REDITALIC}${lib}${NC}"
+					BROKEN="true" # to avoid packages being listed in the brokenpkg array several times
+				fi
+				lib=""
 			fi
 		done
 
